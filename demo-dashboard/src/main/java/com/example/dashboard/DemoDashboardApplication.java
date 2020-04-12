65ff2801c0823774c847f71160346e4461efd950
yyyyccccc121 <1360967364@qq.com>
2020-04-12 13:42:00 +0800

    第一次尝试提交

---
+++ demo-dashboard/src/main/java/com/example/dashboard/DemoDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.example.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DemoDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDashboardApplication.class, args);
+    }
+
+}
